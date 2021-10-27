be16a1b34d4c5fd24a57134306810ed5510e25b7
lephuongthao1306@gmail.com <lephuongthao1306@gmail.com>
2021-10-27 10:07:10 +0700

    'fix-search-function'

--- src/Cocktail/index.js
+++ src/Cocktail/index.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 function Cocktail(props) {
   let [drinks, setDrinks] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     let cocktailName = e.target[0].value;
@@ -10,7 +11,6 @@ function Cocktail(props) {
       url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
     })
       .then((res) => {
-        console.log(res);
         setDrinks((drinks = res.data.drinks));
       })
       .catch((err) => {
@@ -18,26 +18,24 @@ function Cocktail(props) {
       });
   };
 
-  //   const renderIngredient = (drink) => {
-  //     let ingredientArr = [];
-  //     for (let i = 0; i < 14; i++) {
-  //       if (drink[`strIngredient${i}`]) {
-  //         let ingredient = drink[`strIngredient${i}`];
-  //         let measure = drink[`strMeasure${i}`];
-  //         ingredientArr.push({ ingredient, measure });
-  //       }
-  //     }
-  //     console.log(ingredientArr);
-  //     ingredientArr.map((item) => {
-  //       console.log("a");
-  //       return (
-  //         <tr>
-  //           <td>{item.ingredient}</td>
-  //           <td>{item.measure}</td>
-  //         </tr>
-  //       );
-  //     });
-  //   };
+  const renderIngredient = (drink) => {
+    let ingredientArr = [];
+    for (let i = 0; i < 14; i++) {
+      if (drink[`strIngredient${i}`]) {
+        let ingredient = drink[`strIngredient${i}`];
+        let measure = drink[`strMeasure${i}`];
+        ingredientArr.push({ ingredient, measure });
+      }
+    }
+    return ingredientArr.map((item) => {
+      return (
+        <tr>
+          <td>{item.ingredient}</td>
+          <td>{item.measure}</td>
+        </tr>
+      );
+    });
+  };
 
   return (
     <div>
@@ -57,40 +55,50 @@ function Cocktail(props) {
 
         <button className="btn btn-success">search</button>
       </form>
+      {drinks ? (
+        <div className="container my-3">
+          <div className="row">
+            {drinks.map((drink, index) => {
+              return (
+                <div className="col-md-4 col-sm-6 my-3" key={index}>
+                  <div className="card">
+                    <div className="card-body" style={{ height: 130 }}>
+                      <h4 className="card-title">{drink.strDrink}</h4>
+                      <h6 className="card-subtitle text-muted">
+                        {drink.strCategory}
+                      </h6>
+                    </div>
+                    <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+                    <div
+                      className="card-body"
+                      style={{ height: 240, overflow: "auto" }}
+                    >
+                      <p className="card-text">
+                        <b>{drink.strGlass}</b>
+                      </p>
+                      <p className="card-text">{drink.strInstructions}</p>
 
-      <div className="container my-3">
-        <div className="row">
-          {drinks.map((drink, index) => {
-            return (
-              <div className="col-md-4 col-sm-6 my-3" key={index}>
-                <div className="card">
-                  <div className="card-body" style={{ height: 130 }}>
-                    <h4 className="card-title">{drink.strDrink}</h4>
-                    <h6 className="card-subtitle text-muted">
-                      {drink.strCategory}
-                    </h6>
-                  </div>
-                  <img src={drink.strDrinkThumb} alt={drink.strDrink} />
-                  <div className="card-body" style={{ height: 240, overflow: "hidden" }}>
-                    <p className="card-text"><b>{drink.strGlass}</b></p>
-                    <p className="card-text">{drink.strInstructions}</p>
-
-                    {/* <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">Ingredient</th>
-                          <th scope="col">Measure</th>
-                        </tr>
-                      </thead>
-                      <tbody>{renderIngredient(drink)}</tbody>
-                    </table> */}
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">Ingredient</th>
+                            <th scope="col">Measure</th>
+                          </tr>
+                        </thead>
+                        <tbody>{renderIngredient(drink)}</tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : (
+        <h6 className="text-center my-5">
+          Not have that drink! Please check the name and searching again
+        </h6>
+      )}
     </div>
   );
 }
